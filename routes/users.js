e1e9e1ae3757ba165efb5caa63047846bede2b98
rosieamphlett <you@example.com>
2018-08-27 18:01:38 +0100

    again

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const usersRouter = require("express").Router();
 const { getUserByUsername, getAllUsers } = require("../controllers/users");
 
 usersRouter.route("/:username").get(getUserByUsername);
-userRouter.get("/", getAllUsers);
+usersRouter.get("/", getAllUsers);
 
 module.exports = usersRouter
